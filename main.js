1a2e7c4acdb354b03ada89972ee64dd6c6cacb2f
Craftingexpert1 <dev.velp@gmail.com>
2019-06-04 21:21:50 -0400

    switching to canvas

--- main.js
+++ main.js
@@ -1,160 +1,82 @@
-function World(width, height, x, y) {
-    this.width = width || 500;
-    this.height = height || 500;
-    this.x = x || 0;
-    this.y = y || 0;
-    this.el = document.createElement('div');
-    this.el.style.width = this.width + 'px';
-    this.el.style.height = this.height + 'px';
-    this.el.style.position = "absolute";
-    this.el.style.top = this.y + "px";
-    this.el.style.left = this.x + "px";
-    this.things = [];
-    localworld = this;
-    this.Rect = function (x, y, w, h, deg, color, name) {
-        this.x = x || 0;
-        this.y = y || 0;
-        this.deg = deg || 0;
-        this.width = w || 50;
-        this.height = h || 50;
-        this.color = color || 'black';
-        this.name = name;
-        this.el = document.createElement('span');
-        this.el.style.width = this.width + 'px';
-        this.el.style.height = this.height + 'px';
-        this.el.style.fontSize = "0.1px";
-        this.el.style.background = this.color;
-        this.el.style.position = "absolute";
-        localworld.el.appendChild(this.el);
-        localworld.things.push(this);
-        this.isCollided = (rect1, rect2) => {
-            if (rect1.x < rect2.x + rect2.width && rect1.x + rect1.width > rect2.x && rect1.y < rect2.y + rect2.height && rect1.y + rect1.height > rect2.y) {
-                return true;
-            }
-            return false;
-        }
-        (this.transform = () => {
-            for (i = 0; i < localworld.things.length; i++) {
-                this.el.style.transform = "rotate(" + this.deg + "deg) translate(" + this.x + "px," + this.y + "px)";
-                this.el.style.transformOrigin = this.x + this.width / 2 + 'px ' + this.y + this.height / 2 + 'px';
-            }
-        })();
-        (this.turn = (deg) => {
-            this.deg += deg;
-            this.transform();
-        }
-        )(0);
-        (this.movex = (move) => {
-            if (this.x + move + this.width < localworld.width && this.x + move > 0) {
-                this.x += move;
-                this.transform()
-            } else if (this.x + move >= localworld.width) {
-                this.x = localworld.width - this.width;
-                this.transform();
-            } else if (this.x + move <= 0) {
-                this.x = 0;
-                this.transform();
-            }
-        }
-        )(0);
-        (this.movey = (move) => {
-            if (this.y + move + this.height < localworld.height && this.y + move > 0) {
-                this.y += move;
-                this.transform()
-            } else if (this.y + move >= localworld.height) {
-                this.y = localworld.height - this.height;
-                this.transform();
-            } else if (this.y + move <= 0) {
-                this.y = 0;
-                this.transform();
-            }
-        }
-        )(0);
-    }
-    this.Critter = function (x, y, w, h, deg, inside, name) {
-        this.width = w || 50;
-        this.height = h || 50;
-        this.el = document.createElement('span');
-        this.el.style.width = this.width + 'px';
-        this.el.style.height = this.height + 'px';
-        this.name = name;
-        this.x = x;
-        this.y = y;
-        this.deg = deg;
-        this.el.style.position = 'absolute';
-        this.el.style.fontSize = "36px";
-        this.el.style.lineHeight = "50px";
-        this.el.style.textAlign = "center";
-        this.el.style.transition = "150ms linear";
-        this.el.style.userSelect = "none";
-        localworld.el.appendChild(this.el);
-        localworld.things.push(this);
-        this.el.innerHTML = inside || Math.random().toString(36).substring(7).charAt(1);
-        this.stop = false;
-        this.delete = () => this.el.parentElement.removeChild(this.el);
-        this.distance = function (entity) {
-            var x = entity.x - this.x;
-            var y = entity.y - this.y;
-            var result = {
-                distance: Math.hypot(x, y),
-                x: x,
-                y: y
-            };
-            return result;
-        }
-        this.isCollided = (rect1, rect2) => {
-            if (rect1.x < rect2.x + rect2.width && rect1.x + rect1.width > rect2.x && rect1.y < rect2.y + rect2.height && rect1.y + rect1.height > rect2.y) {
-                return true;
-            }
-            return false;
-        }
-        (this.transform = () => {
-            this.el.style.transform = "rotate(" + this.deg + "deg) translate(" + this.x + "px," + this.y + "px)";
-            this.el.style.transformOrigin = (this.x + this.width / 2) + 'px ' + (this.y + this.height / 2) + 'px'
-        }
-        )();
-        (this.turn = (deg) => {
-            this.deg += deg;
-            this.transform();
-        }
-        )(0);
-        (this.movex = (move) => {
-            if (this.x + move + this.width < localworld.width && this.x + move > 0 && !this.stop) {
-                this.x += move;
-                this.transform()
-            } else if (this.x + move >= localworld.width) {
-                this.x = localworld.width - this.width;
-                this.transform();
-            } else if (this.x + move <= 0) {
-                this.x = 0;
-                this.transform()
-            }
-        }
-        )(0);
-        (this.movey = (move) => {
-            if (this.y + move + this.height < localworld.height && this.y + move > 0 && !this.stop) {
-                this.y += move;
-                this.transform()
-            } else if (this.y + move >= localworld.height) {
-                this.y = localworld.height - this.height;
-                this.transform();
-            } else if (this.y + move <= 0) {
-                this.y = 0;
-                this.transform()
-            }
-        }
-        )(0)
-        this.rove = (intensity) => {
-            this.movex(Math.random() * intensity - (intensity / 2));
-            this.movey(Math.random() * (intensity) - (intensity / 2));
-        }
-        this.follow = function (entity, speed) {
-            var dist = this.distance(entity);
-            this.movex(dist.x / speed);
-            this.movey(dist.y / speed);
-            this.transform();
-        }
-        this.activate = () => {
-        }
-    }
+function World(parent, bgcolor, width, height, camx, camy) {
+  var that = this;
+  this.cam = {};
+  this.cam.x = camx || 0;
+  this.cam.y = camy || 0;
+  this.bgcolor = bgcolor || 'white';
+  this.width = width || 500;
+  this.height = width || 500;
+  this.el = document.createElement('canvas');
+  this.el.width = this.width;
+  this.el.height = this.height;
+  this.parent = parent || document.body;
+  this.parent.appendChild(this.el);
+  this.context = this.el.getContext('2d');
+  this.objects = new Map(); //where all the objects go
+  this.keys = {};
+  document.addEventListener('keydown', e=>this.keys[e.key.toLowerCase()]=e.type=true);
+  document.addEventListener('keyup', e=>this.keys[e.key.toLowerCase()]=false);
+  this.Rectangle = function(name, x, y, width, height, color, mass) {
+    this.name = name;
+    this.x = x;
+    this.type = 'rectangle';
+    this.y = y;
+    this.width = width;
+    this.mass = mass || 1;
+    this.acceleration = {x: 0,y: 0};
+    this.height = height;
+    this.color = color;
+  }
+  this.Circle = function(name, x, y, radius, color, mass) {
+    this.name = name;
+    this.mass = mass || 1;
+    this.x = x;
+    this.type = 'circle';
+    this.y = y;
+    this.radius = radius;
+    this.acceleration = {x: 0,y: 0};
+    this.color = color;
+  }
+  this.Image = function(name, x, y, src, width, height, color, mass) {
+    this.name = name;
+    this.x = x;
+    this.mass = mass || 1;
+    this.y = y;
+    this.src = src;
+    this.type = 'image';
+    this.acceleration = {x: 0,y: 0};
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
+  this.set = (obj) => that.objects.set(obj.name, obj);  //set the object
+  this.get = (name) => {return that.objects.get(name)}; //get the object
+  this.update = function() {/*Happens every second*/};
+  this.drawFrame = function() { //Also happens every second
+    that.context.beginPath();
+    that.context.fillStyle = that.bgcolor;
+    that.context.fillRect(0, 0, that.width, that.height);
+    that.context.fill();
+    that.objects.forEach(function(obj) {
+      if (obj.type == 'circle') {
+        that.context.beginPath();
+        that.context.fillStyle = obj.color;
+        that.context.arc(obj.x - that.cam.x, obj.y - that.cam.y, obj.radius, 0, 6.283185);
+        that.context.closePath();
+      }
+      if (obj.type == 'rectangle') {
+        that.context.beginPath();
+        that.context.fillStyle = obj.color;
+        that.context.fillRect(obj.x - that.cam.x, obj.y - that.cam.y, obj.width, obj.height);
+        that.context.closePath();
+      }
+    });
+    requestAnimationFrame(that.drawFrame);
+  }
+  this.animationFrame = function () {
+    that.drawFrame();
+    that.update();
+    requestAnimationFrame(that.animationFrame);
+  }
+  this.start = () => requestAnimationFrame(that.animationFrame);
 }
